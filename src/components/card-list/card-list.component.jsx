45b1a7032af6bb23f14534db94abbf6ea02e27aa
abel <abel.cuza07@gmail.com>
2020-06-05 14:24:18 -0400

    Created the CardList component and the Card component and their styles.css. Finished the Card component.

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import './card-list.styles.css'
+import {Card} from "../card/card.component";
+
+export const CardList = props => (
+    <div className='card-list'>{props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster}/>
+    ))}
+    </div>
+);
